e267e75588ff3bc6a2827b74c69fd71f868f22c3
swathiravi-26 <72958094+swathiravi-26@users.noreply.github.com>
2021-02-18 14:48:19 -0500

    Add files via upload

---
+++ reducer2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import re
+from collections import Counter
+ 
+word = {}
+ 
+
+for line in sys.stdin:
+	line = line.strip()
+	trigram, count= line.split('\t',1)
+	count = int(count)
+	word[trigram] = word.get(trigram,0) + count
+	
+result = Counter(word)
+tops = result.most_common(10)
+for top in tops:
+	print("%s\t%s"%(top[0],top[1]))
